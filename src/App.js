5adb04b7b62a19b4c48420a2e3e0b611df5edc4c
feven tsegay <feventsegay6@gmail.com>
2019-10-09 13:06:57 -0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Snake from './snake'
+import Food from './food'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y =  Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [x,y]
+}
+
+class App extends React.Component {
+  
+  state = {
+    food: getRandomCoordinates(),
+    snakeDots: [
+      [0,0],
+      [0,2]
+    ]
+  }
+
+
+  render(){
+
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        {/* <Snake/>
+        <Food/> */}
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
